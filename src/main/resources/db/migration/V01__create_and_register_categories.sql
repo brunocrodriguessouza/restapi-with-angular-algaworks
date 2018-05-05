039e715ce6b467b75cc152c0d88602f716924896
Bruno <brunoprogramadorjava@gmail.com>
2018-05-05 00:08:18 -0300

    Data migration with flyway

---
+++ src/main/resources/db/migration/V01__create_and_register_categories.sql
@@ -0,0 +1,14 @@
+CREATE TABLE category (
+    id BIGINT(20) PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+INSERT INTO category (name) values ('Recreation');
+INSERT INTO category (name) values ('Food');
+INSERT INTO category (name) values ('Supermarket');
+INSERT INTO category (name) values ('Drugstore');
+INSERT INTO category (name) values ('Others');
+
+
+
