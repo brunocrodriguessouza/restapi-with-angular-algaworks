64d3ebe3b848ffeb0293dc9f1e33784fabd4bb90
Bruno <brunoprogramadorjava@gmail.com>
2018-05-08 17:49:35 -0300

    Using Events to add Header Location

--- src/main/java/com/algaworks/algamoneyapi/controller/CategoryController.java
+++ src/main/java/com/algaworks/algamoneyapi/controller/CategoryController.java
@@ -1,12 +1,12 @@
 package com.algaworks.algamoneyapi.controller;
 
-import java.net.URI;
 import java.util.List;
 
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,10 +14,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.algaworks.algamoneyapi.event.ResourceCreatedEvent;
 import com.algaworks.algamoneyapi.model.Category;
 import com.algaworks.algamoneyapi.repository.CategoryRepository;
 
@@ -27,6 +26,9 @@ public class CategoryController {
 
 	@Autowired
 	private CategoryRepository categoryRepository;
+
+	@Autowired
+	private ApplicationEventPublisher publisher;
 
 	@GetMapping
 	public List<Category> getAll() {
@@ -42,16 +44,7 @@ public class CategoryController {
 	public ResponseEntity<Category> insertCategory(@Valid @RequestBody Category category,
 			HttpServletResponse response) {
 		Category savedCategory = categoryRepository.save(category);
-		
-		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri()
-				.path("/{id}")
-				.buildAndExpand(savedCategory.getId())
-				.toUri();
-		
-		response.setHeader("Location", uri.toASCIIString());
-
-		return ResponseEntity.created(uri).body(savedCategory);
-
+		publisher.publishEvent(new ResourceCreatedEvent(this, response, savedCategory.getId()));
+		return ResponseEntity.status(HttpStatus.CREATED).body(savedCategory);
 	}
-
 }
--- src/main/java/com/algaworks/algamoneyapi/controller/PeopleController.java
+++ src/main/java/com/algaworks/algamoneyapi/controller/PeopleController.java
@@ -1,12 +1,13 @@
 package com.algaworks.algamoneyapi.controller;
 
-import java.net.URI;
 import java.util.List;
 
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,8 +15,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.algaworks.algamoneyapi.event.ResourceCreatedEvent;
 import com.algaworks.algamoneyapi.model.People;
 import com.algaworks.algamoneyapi.repository.PeopleRepository;
 
@@ -25,6 +26,9 @@ public class PeopleController {
 
 	@Autowired
 	private PeopleRepository peopleRepository;
+
+	@Autowired
+	private ApplicationEventPublisher publisher;
 
 	@GetMapping
 	public List<People> getAll() {
@@ -39,16 +43,7 @@ public class PeopleController {
 	@PostMapping
 	public ResponseEntity<People> InsertPeople(@Valid @RequestBody People people, HttpServletResponse response) {
 		People savedPeople = peopleRepository.save(people);
-		
-		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri()
-				.path("/{id}")
-				.buildAndExpand(savedPeople.getId())
-				.toUri();
-
-		response.setHeader("Location", uri.toASCIIString());
-
-		return ResponseEntity.created(uri).body(savedPeople);
-
+		publisher.publishEvent(new ResourceCreatedEvent(this, response, savedPeople.getId()));
+		return ResponseEntity.status(HttpStatus.CREATED).body(savedPeople);
 	}
-
 }
