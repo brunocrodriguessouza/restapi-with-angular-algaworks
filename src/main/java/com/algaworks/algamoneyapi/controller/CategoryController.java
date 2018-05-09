bbe0e828a342e0d17b3b3d5e3215a188e1d0691b
Bruno <brunoprogramadorjava@gmail.com>
2018-05-09 13:36:58 -0300

    Implementing endpoint to delete people and categories

--- src/main/java/com/algaworks/algamoneyapi/controller/CategoryController.java
+++ src/main/java/com/algaworks/algamoneyapi/controller/CategoryController.java
@@ -9,11 +9,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.algaworks.algamoneyapi.event.ResourceCreatedEvent;
@@ -31,20 +33,26 @@ public class CategoryController {
 	private ApplicationEventPublisher publisher;
 
 	@GetMapping
-	public List<Category> getAll() {
+	public List<Category> getAll(){
 		return categoryRepository.findAll();
 	}
 
 	@GetMapping("/{id}")
-	public Category getById(@PathVariable Long id) {
+	public Category getById(@PathVariable Long id){
 		return categoryRepository.findOne(id);
 	}
 
 	@PostMapping
 	public ResponseEntity<Category> insertCategory(@Valid @RequestBody Category category,
-			HttpServletResponse response) {
+			HttpServletResponse response){
 		Category savedCategory = categoryRepository.save(category);
 		publisher.publishEvent(new ResourceCreatedEvent(this, response, savedCategory.getId()));
 		return ResponseEntity.status(HttpStatus.CREATED).body(savedCategory);
+	}
+	
+	@DeleteMapping("/{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void remove(@PathVariable Long id){
+		categoryRepository.delete(id);
 	}
 }
--- src/main/java/com/algaworks/algamoneyapi/controller/PeopleController.java
+++ src/main/java/com/algaworks/algamoneyapi/controller/PeopleController.java
@@ -9,11 +9,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.algaworks.algamoneyapi.event.ResourceCreatedEvent;
@@ -31,12 +33,12 @@ public class PeopleController {
 	private ApplicationEventPublisher publisher;
 
 	@GetMapping
-	public List<People> getAll() {
+	public List<People> getAll(){
 		return peopleRepository.findAll();
 	}
 
 	@GetMapping("/{id}")
-	public People getById(@PathVariable Long id) {
+	public People getById(@PathVariable Long id){
 		return peopleRepository.findOne(id);
 	}
 
@@ -45,5 +47,11 @@ public class PeopleController {
 		People savedPeople = peopleRepository.save(people);
 		publisher.publishEvent(new ResourceCreatedEvent(this, response, savedPeople.getId()));
 		return ResponseEntity.status(HttpStatus.CREATED).body(savedPeople);
+	}
+	
+	@DeleteMapping("/{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void remove(@PathVariable Long id){
+		peopleRepository.delete(id);
 	}
 }
