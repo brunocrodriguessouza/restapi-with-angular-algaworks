ac2e4d02a38558f43589b9b95f7c4471595814f5
Bruno <brunoprogramadorjava@gmail.com>
2018-05-12 10:24:54 -0300

    Creating ExpenseController and ExpenseService

---
+++ src/main/java/com/algaworks/algamoneyapi/controller/ExpenseController.java
@@ -0,0 +1,68 @@
+package com.algaworks.algamoneyapi.controller;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.algaworks.algamoneyapi.event.ResourceCreatedEvent;
+import com.algaworks.algamoneyapi.model.Expense;
+import com.algaworks.algamoneyapi.repository.ExpenseRepository;
+import com.algaworks.algamoneyapi.service.ExpenseService;
+
+@RestController
+@RequestMapping("/expenses")
+public class ExpenseController {
+
+	@Autowired
+	private ExpenseRepository expenseRepository;
+
+	@Autowired
+	private ExpenseService expenseService;
+
+	@Autowired
+	private ApplicationEventPublisher publisher;
+
+	@GetMapping
+	public List<Expense> getAll() {
+		return expenseRepository.findAll();
+	}
+
+	@GetMapping("/{id}")
+	public Expense getById(@PathVariable Long id) {
+		return expenseRepository.findOne(id);
+	}
+
+	@PostMapping
+	public ResponseEntity<Expense> InsertExpense(@Valid @RequestBody Expense expense, HttpServletResponse response) {
+		Expense savedExpense = expenseRepository.save(expense);
+		publisher.publishEvent(new ResourceCreatedEvent(this, response, savedExpense.getId()));
+		return ResponseEntity.status(HttpStatus.CREATED).body(savedExpense);
+	}
+
+	@DeleteMapping("/{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void remove(@PathVariable Long id) {
+		expenseRepository.delete(id);
+	}
+
+	@PutMapping("/{id}")
+	public ResponseEntity<Expense> update(@PathVariable Long id, @Valid @RequestBody Expense expense) {
+		Expense savedExpense = expenseService.update(id, expense);
+		return ResponseEntity.ok(savedExpense);
+	}
+}
---
+++ src/main/java/com/algaworks/algamoneyapi/service/ExpenseService.java
@@ -0,0 +1,23 @@
+package com.algaworks.algamoneyapi.service;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+
+import com.algaworks.algamoneyapi.model.Expense;
+import com.algaworks.algamoneyapi.repository.ExpenseRepository;
+
+public class ExpenseService {
+
+	@Autowired
+	ExpenseRepository expenseRepository;
+
+	public Expense update(Long id, Expense expense) {
+		Expense savedExpense = expenseRepository.findOne(id);
+		if (savedExpense == null) {
+			throw new EmptyResultDataAccessException(1);
+		}
+		BeanUtils.copyProperties(expense, savedExpense, "id");
+		return expenseRepository.save(savedExpense);
+	}
+}
